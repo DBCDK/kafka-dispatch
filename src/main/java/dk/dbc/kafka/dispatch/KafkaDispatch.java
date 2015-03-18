f8b0848699a2f75a47ae4e76f11424f033b17a47
Adam Tulinius <atu@dbc.dk>
2015-03-18 03:05:20 +0100

    default to reading input as UTF-8

--- src/main/java/dk/dbc/kafka/dispatch/KafkaDispatch.java
+++ src/main/java/dk/dbc/kafka/dispatch/KafkaDispatch.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Properties;
 
@@ -51,7 +52,7 @@ public class KafkaDispatch {
                 = new KafkaProducer<>(properties, stringSerializer, stringSerializer);
 
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
             String line;
             int count = 0;
 
